0ad8eb9e9f1781cf596cee86cb9586cbe662f8ce
StudioCrew-design <dev.studiocrew@gmail.com>
2023-06-24 19:59:20 +0530

    created multi cell pdf successfully with content

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 import glob
 from pathlib import Path
 
+
 filepaths = glob.glob("textfiles/*.txt")
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 for filepath in filepaths:
@@ -10,5 +11,11 @@ for filepath in filepaths:
     pdf.add_page()
     pdf.set_font(family="Times", style="B", size=15)
     pdf.cell(w=0, h=15, txt=f"{name}", ln=1)
+    with open(filepath) as file:
+        content = file.read()
+    pdf.set_font(family="Times", style="B", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.multi_cell(w=0, h=8, txt=content)
+
 
 pdf.output("output.pdf")
